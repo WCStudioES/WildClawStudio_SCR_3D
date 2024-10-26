774345252c21f360346171fd6225a5f3106f6805
jagonmes <65248503+jagonmes@users.noreply.github.com>
2024-10-26 13:57:33 +0200

    Arreglo a la selección de controles

---
+++ Assets/Scripts/Jugador/SelectPlayerInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SelectPlayerInput : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    private void Start()
+    {
+
+        // Asigna el esquema de control en función del dispositivo detectado
+        if (Gamepad.current != null)
+        {
+            playerInput.SwitchCurrentControlScheme("Gamepad", Gamepad.current);
+        }
+        else if (Touchscreen.current != null)
+        {
+            playerInput.SwitchCurrentControlScheme("Touch", Touchscreen.current);
+        }
+        else
+        {
+            playerInput.SwitchCurrentControlScheme("Keyboard&Mouse");
+        }
+    }
+}
