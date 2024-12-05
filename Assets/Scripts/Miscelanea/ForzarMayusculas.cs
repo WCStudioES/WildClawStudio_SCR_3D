238839bab113105c1a6f5bb38a7f9e46a8495b82
jagonmes <65248503+jagonmes@users.noreply.github.com>
2024-12-05 18:39:21 +0100

    Versión 2.2.6: Forzadas mayúsculas en input del servidor, actualizado limite de caracteres para usuario y contraseña

---
+++ Assets/Scripts/Miscelanea/ForzarMayusculas.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForzarMayusculas : MonoBehaviour
+{
+    public void forzarMayusculas(string s)
+    {
+        s = s.ToUpper();
+    }
+}
