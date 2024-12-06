0477d57ad095958dc6d0ea805a8f515d8bd022a2
Jesús Bastante <jesusbaslop24@gmail.com>
2024-12-06 19:27:01 +0100

    uitutorial

--- Assets/Scripts/Jugador/UITutorial.cs
+++ Assets/Scripts/Jugador/UITutorial.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class UITutorial : MonoBehaviour
+public class UITutorial : NetworkBehaviour
 {
-    public void CambiarAInstrucciones2()
+    [SerializeField] private GameObject uiTutorial;
+    [SerializeField] private GameObject[] uiPantallas;
+    private int index = 0;
+    
+    public void CambiarPantalla(bool isNext)
     {
         if (IsOwner)
         {
-            Instrucciones.SetActive(false);
-            Instrucciones2.SetActive(true);
+            uiPantallas[index].SetActive(false);
+            if (isNext)
+            {
+                if (index == uiPantallas.Length - 1)
+                    index = 0;
+                else
+                    index++;
+                
+                uiPantallas[index].SetActive(true);
+            }
+
+            else
+            {
+                if (index == uiPantallas.Length - 1)
+                    index = 0;
+                else
+                    index++;
+                
+                uiPantallas[index].SetActive(true);
+            }
+  
         }
     }
 }
